68027c9c3e4cf7552f4326055146c80f3d381a8a
wwiecek <witold.wiecek@gmail.com>
2019-05-15 17:30:40 +0100

    extra documentation

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,8 @@
 .onUnload <- function (libpath) {
   library.dynam.unload("rgraphsampler", libpath)
 }
+
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Welcome to rgraphsampler -- prototype version -- May 2019")
+  packageStartupMessage("Use vignette('rgraphsampler') for tutorial.")
+}
